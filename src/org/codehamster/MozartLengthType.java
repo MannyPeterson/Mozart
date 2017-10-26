d25cb8cbefe65bfac06b3f0c569a5bee5921654e
manny <manny@192.168.1.105>
2017-10-25 19:17:40 -0500

    update

---
+++ src/org/codehamster/MozartLengthType.java
@@ -0,0 +1,58 @@
+package org.codehamster;
+
+public enum MozartLengthType {
+	SIXTEENTH("1/16", 2),
+	EIGHTH("1/8", 4),
+	QUARTER("1/4", 8),
+	HALF("1/2", 16),
+	WHOLE("1/1", 32);
+
+	private String string;
+	private int value;
+
+	private MozartLengthType(String note, int value) throws MozartRuntimeException {
+		try {
+			this.setString(note);
+			this.setValue(value);
+			return;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		}
+	}
+	
+	public String getString() {
+		return this.string;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	private void setString(String note) throws MozartRuntimeException {
+		try {
+			if (note == null) {
+				throw new MozartRuntimeException(this.getClass().getName() + ": string is null.");
+			}
+			this.string = note;
+			return;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		}
+	}
+	
+	private void setValue(int value) throws MozartRuntimeException {
+		try {
+			if (value < 0 | value > 2) {
+				throw new MozartRuntimeException(this.getClass().getName() + ": value out of range.");
+			}
+			this.value = value;
+			return;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		}
+	}
+
+	public String toString() {
+		return this.getString();
+	}
+}
