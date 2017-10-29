8253e2cd4836d39e8c7b93fa33bea268c5cc6311
Manny Peterson <me@mannypeterson.com>
2017-10-29 14:57:40 -0500

    update

--- src/org/codehamster/MozartMain.java
+++ src/org/codehamster/MozartMain.java
@@ -1,32 +1,66 @@
+/*
+ * 
+ *  Mozart Digital Composer Version 0.1
+ *  Copyright (C) 2017 Manny Peterson <me@mannypeterson.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
 package org.codehamster;
 
 import javax.sound.midi.InvalidMidiDataException;
 
 public class MozartMain {
-	public static void main(String[] args) throws InvalidMidiDataException, InterruptedException {
-		System.out.println("Mozart Digital Composer v0.1 (C)Copyright 2017 Manny Peterson All Rights Reserved");
-
-		MozartScale scale = new MozartScale(MozartScaleType.MAJOR, MozartPitchType.C_SHARP_D_FLAT);
+	public static void main(String[] args) {
+		System.out.println("Mozart Digital Composer Version 0.1");
+		System.out.println("Copyright (C) 2017 Manny Peterson <me@mannypeterson.com>");
+		System.out.println("");
+		System.out.println("This program is free software: you can redistribute it and/or modify");
+		System.out.println("it under the terms of the GNU General Public License as published by");
+		System.out.println("the Free Software Foundation, either version 3 of the License, or");
+		System.out.println("(at your option) any later version.");
+		System.out.println("");
+		System.out.println("This program is distributed in the hope that it will be useful,");
+		System.out.println("but WITHOUT ANY WARRANTY; without even the implied warranty of");
+		System.out.println("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the");
+		System.out.println("GNU General Public License for more details.");
+		System.out.println("");
+		System.out.println("You should have received a copy of the GNU General Public License");
+		System.out.println("along with this program.  If not, see <https://www.gnu.org/licenses/>.");
 		
-		
-		MozartPhrase phrase1 = new MozartPhrase(scale, MozartOctaveType.SIXTH, 100);
-		MozartPhrase phrase2 = new MozartPhrase(scale, MozartOctaveType.SEVENTH, 100);
-		MozartPhrase phrase3 = new MozartPhrase(scale, MozartOctaveType.SIXTH, 100);
-		MozartPhrase phrase4 = new MozartPhrase(scale, MozartOctaveType.SIXTH, 100);
-
-
-		MozartArrangement arrange = new MozartArrangement(10);
-		
-		arrange.addPhrase(phrase1);
-		arrange.addPhrase(phrase2);
-		arrange.addPhrase(phrase3);
-		arrange.addPhrase(phrase4);
-		arrange.create();
-		
-		MozartInstrument inst = new MozartInstrument();
-		
-		for(MozartPhrase playPhrase: arrange.getArrangement()) {
-			inst.play(playPhrase);			
+		MozartScale scale;
+		MozartArrangement arrangement;
+		MozartInstrument instrument;
+		try {
+			scale = new MozartScale(MozartScaleType.MAJOR, MozartPitchType.C_SHARP_D_FLAT);
+			arrangement = new MozartArrangement(10);
+			instrument = new MozartInstrument();
+			for (int i = 0; i < 6; i++) {
+				arrangement.addPhrase(new MozartPhrase(scale, MozartOctaveType.SIXTH, 60));
+			}
+			arrangement.create();
+			for (MozartPhrase phrase : arrangement.getArrangement()) {
+				instrument.play(phrase);
+			}
+			return;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		} catch (InvalidMidiDataException e) {
+			throw new MozartRuntimeException(e);
+		} catch (InterruptedException e) {
+			throw new MozartRuntimeException(e);
 		}
 	}
 }
