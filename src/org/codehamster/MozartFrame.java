6b9e494176e2a2bf1204c593fdad33b6cc1414a0
Manny Peterson <me@mannypeterson.com>
2017-11-04 14:50:39 -0500

    update

--- src/org/codehamster/MozartFrame.java
+++ src/org/codehamster/MozartFrame.java
@@ -50,165 +50,237 @@ public class MozartFrame extends JFrame {
 	private JTextField txtPhraseLength;
 	private JTextField txtSongLength;
 
-	public MozartFrame() {
-		/*
-		 * 
-		 * THIS CODE IS A MESS AT THE MOMENT - IT WILL BE REFINED
-		 * 
-		 */
-		setTitle("Mozart Digital Composer");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 560);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public MozartFrame() throws MozartRuntimeException {
+		String[] scaleItems = { "MAJOR", "MINOR" };
+		String[] keyItems = { "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B" };
+		JLabel lblMozartImg;
+		JLabel lblScale;
+		JLabel lblKey;
+		JLabel lblPhraseLength;
+		JLabel lblSongLength;
+		JButton btnExit;
+		JButton btnPlay;
+		try {
+			setTitle("Mozart Digital Composer");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 800, 560);
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
 
-		JLabel lblMozartImg = new JLabel("");
-		lblMozartImg.setIcon(new ImageIcon(MozartFrame.class.getResource("/org/codehamster/Mozart.png")));
-		lblMozartImg.setBounds(6, 6, 185, 257);
-		contentPane.add(lblMozartImg);
+			lblMozartImg = new JLabel("");
+			lblMozartImg.setBounds(6, 6, 185, 257);
+			lblMozartImg.setIcon(new ImageIcon(MozartFrame.class.getResource("/org/codehamster/Mozart.png")));
+			contentPane.add(lblMozartImg);
 
-		JLabel lblScale = new JLabel("Scale");
-		lblScale.setBounds(197, 266, 90, 22);
-		contentPane.add(lblScale);
+			lblScale = new JLabel("Scale");
+			lblScale.setBounds(197, 266, 90, 22);
+			contentPane.add(lblScale);
 
-		JLabel lblKey = new JLabel("Key");
-		lblKey.setBounds(356, 266, 90, 22);
-		contentPane.add(lblKey);
+			lblKey = new JLabel("Key");
+			lblKey.setBounds(356, 266, 90, 22);
+			contentPane.add(lblKey);
 
-		JLabel lblPhraseLength = new JLabel("Phrase Length (Notes)");
-		lblPhraseLength.setBounds(507, 265, 151, 22);
-		contentPane.add(lblPhraseLength);
+			lblPhraseLength = new JLabel("Phrase Length (Notes)");
+			lblPhraseLength.setBounds(507, 265, 151, 22);
+			contentPane.add(lblPhraseLength);
 
-		JLabel lblSongLength = new JLabel("Song Length (Phrases)");
-		lblSongLength.setBounds(507, 325, 151, 22);
-		contentPane.add(lblSongLength);
+			lblSongLength = new JLabel("Song Length (Phrases)");
+			lblSongLength.setBounds(507, 325, 151, 22);
+			contentPane.add(lblSongLength);
 
-		JButton btnExit = new JButton("Exit");
-		btnExit.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-				System.exit(0);
-			}
-		});
-		btnExit.setBounds(677, 504, 117, 29);
-		contentPane.add(btnExit);
+			btnExit = new JButton("Exit");
+			btnExit.setBounds(677, 504, 117, 29);
+			btnExit.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					System.exit(0);
+				}
+			});
+			contentPane.add(btnExit);
 
-		JButton btnPlay = new JButton("Play");
-		btnPlay.addActionListener(new ActionListener() {
-			private MozartFrame frame;
+			btnPlay = new JButton("Play");
+			btnPlay.setBounds(555, 504, 117, 29);
+			btnPlay.addActionListener(new ActionListener() {
+				private MozartFrame frame;
 
-			public void actionPerformed(ActionEvent ae) {
-				MozartFrame frame = this.frame;
-				new SwingWorker<Void, Void>() {
-					public Void doInBackground() throws MozartRuntimeException {
-						Random random;
-						MozartScale scale;
-						MozartArrangement arrangement;
-						MozartInstrument instrument;
-						try {
-							random = new Random(System.currentTimeMillis());
-							scale = new MozartScale(MozartScaleType.MAJOR, MozartPitchType.D_SHARP_E_FLAT);
-							arrangement = new MozartArrangement(10);
-							instrument = new MozartInstrument();
-							for (int i = 0; i < 6; i++) {
-								arrangement.addPhrase(new MozartPhrase(scale, MozartOctaveType.SIXTH, random, 60));
+				public void actionPerformed(ActionEvent ae)  {
+					MozartFrame frame = this.frame;
+					new SwingWorker<Void, Void>() {
+						public Void doInBackground() {
+							Random random;
+							MozartScale scale;
+							MozartArrangement arrangement;
+							MozartInstrument instrument;
+							try {
+								random = new Random(System.currentTimeMillis());
+								frame.writeConsole("Scale: " + frame.getScale().getString());
+								frame.writeConsole("Key: " + frame.getKey().getString());
+								scale = new MozartScale(frame.getScale(), frame.getKey());
+								arrangement = new MozartArrangement(10);
+								instrument = new MozartInstrument();
+								for (int i = 0; i < 6; i++) {
+									arrangement
+											.addPhrase(new MozartPhrase(scale, MozartOctaveType.SIXTH, random,  60));
+								}
+								arrangement.create();
+								instrument.open();
+								for (MozartPhrase phrase : arrangement.getArrangement()) {
+									instrument.play(phrase, frame);
+								}
+								instrument.close();
+								return null;
+							} catch (MozartRuntimeException e) {
+								e.printStackTrace();
+								return null;
+							} catch (InvalidMidiDataException e) {
+								e.printStackTrace();
+								return null;
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+								return null;
 							}
-							arrangement.create();
-							instrument.open();
-							for (MozartPhrase phrase : arrangement.getArrangement()) {
-								instrument.play(phrase, frame);
-							}
-							instrument.close();
-							return null;
-						} catch (MozartRuntimeException ex) {
-							throw new MozartRuntimeException(ex);
-						} catch (InvalidMidiDataException ex) {
-							throw new MozartRuntimeException(ex);
-						} catch (InterruptedException ex) {
-							throw new MozartRuntimeException(ex);
 						}
+
+						protected void done() {
+
+						}
+					}.execute();
+				}
+
+				public ActionListener setFrame(MozartFrame frame) throws MozartRuntimeException {
+					try {
+						if (frame == null) {
+							throw new MozartRuntimeException(this.getClass().getName() + ": frame is null.");
+						}
+						this.frame = frame;
+						return this;
+					} catch (MozartRuntimeException e) {
+						throw new MozartRuntimeException(e);
 					}
+				}
+			}.setFrame(this));
+			contentPane.add(btnPlay);
 
-					protected void done() {
+			this.txtConsole = new JTextArea();
+			this.txtConsole.setBounds(197, 6, 597, 257);
+			this.txtConsole.setEditable(false);
+			contentPane.add(this.txtConsole);
 
-					}
-				}.execute();
+			this.txtConsoleScrollPane = new JScrollPane(this.txtConsole);
+			this.txtConsoleScrollPane.setBounds(197, 6, 597, 257);
+			this.txtConsoleScrollBar = this.txtConsoleScrollPane.getVerticalScrollBar();
+			contentPane.add(this.txtConsoleScrollPane);
 
+			this.lstScale = new JList(scaleItems);
+			this.lstScale.setBounds(197, 290, 224, 182);
+			this.lstScale.setSelectedIndex(0);
+			contentPane.add(this.lstScale);
+
+			this.lstScaleScrollPane = new JScrollPane(this.lstScale);
+			this.lstScaleScrollPane.setBounds(197, 290, 129, 182);
+			contentPane.add(this.lstScaleScrollPane);
+
+			this.lstKey = new JList(keyItems);
+			this.lstKey.setBounds(454, 290, 224, 182);
+			this.lstKey.setSelectedIndex(0);
+			contentPane.add(this.lstKey);
+
+			this.lstKeyScrollPane = new JScrollPane(this.lstKey);
+			this.lstKeyScrollPane.setBounds(356, 290, 129, 182);
+			contentPane.add(this.lstKeyScrollPane);
+
+			this.txtPhraseLength = new JTextField();
+			this.txtPhraseLength.setBounds(502, 287, 130, 26);
+			this.txtPhraseLength.setText("60");
+			this.txtPhraseLength.setColumns(10);
+			contentPane.add(this.txtPhraseLength);
+
+			this.txtSongLength = new JTextField();
+			this.txtSongLength.setBounds(502, 347, 130, 26);
+			this.txtSongLength.setText("10");
+			this.txtSongLength.setColumns(10);
+			contentPane.add(this.txtSongLength);
+
+			this.writeConsole(
+					"Mozart Digital Composer Version 0.1\nCopyright (C) 2017 Manny Peterson <me@mannypeterson.com>\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
+
+			return;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		}
+	}
+
+	public MozartPitchType getKey() throws MozartRuntimeException {
+		MozartPitchType retVal;
+		String selectedKey;
+		try {
+			selectedKey = this.lstKey.getSelectedValue().toString();
+			if (MozartPitchType.C.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.C;
+			} else if (MozartPitchType.C_SHARP_D_FLAT.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.C_SHARP_D_FLAT;
+			} else if (MozartPitchType.D.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.D;
+			} else if (MozartPitchType.D_SHARP_E_FLAT.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.D_SHARP_E_FLAT;
+			} else if (MozartPitchType.E.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.E;
+			} else if (MozartPitchType.F.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.F;
+			} else if (MozartPitchType.F_SHARP_G_FLAT.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.F_SHARP_G_FLAT;
+			} else if (MozartPitchType.G.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.G;
+			} else if (MozartPitchType.G_SHARP_A_FLAT.getString().equals(selectedKey)) {
+				retVal = MozartPitchType.G_SHARP_A_FLAT;
+			} else {
+				throw new MozartRuntimeException(this.getClass().getName() + ": unsupported pitch type.");
 			}
+			return retVal;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		}
+	}
 
-			public ActionListener setFrame(MozartFrame frame) {
-				this.frame = frame;
-				return this;
+	public MozartScaleType getScale() throws MozartRuntimeException {
+		MozartScaleType retVal;
+		String selectedScale;
+		try {
+			if (this.lstScale == null) {
+				throw new MozartRuntimeException(this.getClass().getName() + ": lstScale is null.");
 			}
-		}.setFrame(this));
-		btnPlay.setBounds(555, 504, 117, 29);
-		contentPane.add(btnPlay);
-
-		this.txtConsole = new JTextArea();
-		this.txtConsole.setEditable(false);
-		this.txtConsole.setBounds(197, 6, 597, 257);
-		contentPane.add(this.txtConsole);
-
-		this.txtConsoleScrollPane = new JScrollPane(this.txtConsole);
-		this.txtConsoleScrollPane.setBounds(197, 6, 597, 257);
-		contentPane.add(this.txtConsoleScrollPane);
-
-		String[] scaleItems = { "Major Scale", "Minor Scale" };
-
-		this.lstScale = new JList(scaleItems);
-		this.lstScale.setBounds(197, 290, 224, 182);
-		this.lstScale.setSelectedIndex(0);
-		contentPane.add(this.lstScale);
-
-		String[] keyItems = { "C", "C# / Db", "D", "D# / Eb", "E", "F", "F# / Gb", "G", "G# / Ab", "A", "A# / Bb",
-				"B" };
-
-		this.lstKey = new JList(keyItems);
-		this.lstKey.setBounds(454, 290, 224, 182);
-		this.lstKey.setSelectedIndex(0);
-		contentPane.add(this.lstKey);
-
-		this.lstScaleScrollPane = new JScrollPane(this.lstScale);
-		this.lstScaleScrollPane.setBounds(197, 290, 129, 182);
-		contentPane.add(this.lstScaleScrollPane);
-
-		this.lstKeyScrollPane = new JScrollPane(this.lstKey);
-		this.lstKeyScrollPane.setBounds(356, 290, 129, 182);
-		contentPane.add(this.lstKeyScrollPane);
-
-		this.txtPhraseLength = new JTextField();
-		this.txtPhraseLength.setText("60");
-		this.txtPhraseLength.setBounds(502, 287, 130, 26);
-		contentPane.add(this.txtPhraseLength);
-		this.txtPhraseLength.setColumns(10);
-
-		this.txtSongLength = new JTextField();
-		this.txtSongLength.setText("10");
-		this.txtSongLength.setColumns(10);
-		this.txtSongLength.setBounds(502, 347, 130, 26);
-		contentPane.add(this.txtSongLength);
-
-		this.txtConsoleScrollBar = this.txtConsoleScrollPane.getVerticalScrollBar();
-
-		this.txtConsole.append(
-				"Mozart Digital Composer Version 0.1\nCopyright (C) 2017 Manny Peterson <me@mannypeterson.com>\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see <https://www.gnu.org/licenses/>.\n");
-
+			selectedScale = this.lstScale.getSelectedValue().toString();
+			if (MozartScaleType.MAJOR.getString().equals(selectedScale)) {
+				retVal = MozartScaleType.MAJOR;
+			} else if (MozartScaleType.MINOR.getString().equals(selectedScale)) {
+				retVal = MozartScaleType.MINOR;
+			} else {
+				throw new MozartRuntimeException(this.getClass().getName() + ": unsupported scale type.");
+			}
+			return retVal;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		}
 	}
 
-	public MozartPitchType getKey() {
-
-		return null;
-	}
-
-	public MozartScaleType getScale() {
-
-		return null;
-	}
-
-	public void writeConsole(String text) {
-		this.txtConsole.append(text + "\n");
-		this.txtConsoleScrollBar.setValue(this.txtConsoleScrollBar.getMaximum());
-		return;
+	public void writeConsole(String text) throws MozartRuntimeException {
+		try {
+			if (text == null) {
+				throw new MozartRuntimeException(this.getClass().getName() + ": text is null.");
+			}
+			if (this.txtConsole == null) {
+				throw new MozartRuntimeException(this.getClass().getName() + ": txtConsole is null.");
+			}
+			if (this.txtConsoleScrollBar == null) {
+				throw new MozartRuntimeException(this.getClass().getName() + ": txtConsoleScrollBar is null.");
+			}
+			this.txtConsole.append(text + "\n");
+			this.txtConsoleScrollBar.setValue(this.txtConsoleScrollBar.getMaximum());
+			return;
+		} catch (MozartRuntimeException e) {
+			throw new MozartRuntimeException(e);
+		}
 	}
 }
